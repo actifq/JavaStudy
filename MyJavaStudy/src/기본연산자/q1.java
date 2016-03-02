cb44d6c2301491ee24c2927af365d1d93b913662
cj lee <actifq@gmail.com>
2016-03-02 15:37:30 +0900

    q1.1

---
+++ MyJavaStudy/src/기본연산자/q1.java
@@ -0,0 +1,16 @@
+package 기본연산자;
+
+// 입력받은 값을 10,000원권, 5,000원권, 1,000원권, 500원권, 100원권, 50원권, 10원권 단위로 환산하여 각각의 개수를 출력하는 프로그램 
+
+import java.util.Scanner;;
+
+public class q1 {
+
+	public static void main(String[] args) {
+		
+				
+		
+
+	}
+
+}
