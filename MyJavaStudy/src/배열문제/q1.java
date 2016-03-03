d485e83680979be4987a0e75c6b47c880ada4243
cj lee <actifq@gmail.com>
2016-03-03 19:56:09 +0900

    16.03.03 문제풀이 추가

---
+++ MyJavaStudy/src/배열문제/q1.java
@@ -0,0 +1,21 @@
+package 배열문제;
+
+import java.util.Scanner;
+
+
+public class q1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("정수 10개 입력:");
+		int ii=sc.nextInt();
+		  
+		
+	}
+
+}
